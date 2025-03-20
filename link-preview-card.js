dc82d68c9d182911c31f128c0a24a0dec54fe973
ChrisSarpong <cjs7724@psu.edu>
2025-03-20 12:42:03 -0400

    removing the demo

--- link-preview-card.js
+++ link-preview-card.js
@@ -1,6 +1,7 @@
 /**
  * Copyright 2025 ChrisSarpong
  * @license Apache-2.0, see LICENSE for full text.
+ * https://github.com/haxtheweb/issues/issues/1764 <- Directions for this component
  */
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
@@ -8,12 +9,11 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `link-preview-card`
- * 
+ *
  * @demo index.html
  * @element link-preview-card
  */
 export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "link-preview-card";
   }
@@ -45,31 +45,43 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--link-preview-card-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(
+            --link-preview-card-label-font-size,
+            var(--ddd-font-size-s)
+          );
+        }
+      `,
+    ];
   }
 
   // Lit render the HTML
   render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    return html` <link-preview-card>
+      <div class="wrapper">
+        <div class="link">
+          <div class="img">
+            <div class="desc">
+              //div for link, five for img and what not
+              <h3><span>${this.t.title}:</span> ${this.title}</h3>
+              <slot></slot>
+            </div>
+          </div>
+        </div></div
+    ></link-preview-card>`;
   }
 
   /**
